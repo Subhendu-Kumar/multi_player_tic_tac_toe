eee823b90a1dda4852a61d3b825ed994c178b244
Subhendu-Kumar <subhendukumardutta330@gmail.com>
2025-11-21 18:03:51 +0530

    initial

---
+++ mpt3_client/lib/screens/main_menu_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainMenuScreen extends StatelessWidget {
+  const MainMenuScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Main Menu')),
+      body: const Center(child: Text('Welcome to the Main Menu!')),
+    );
+  }
+}
